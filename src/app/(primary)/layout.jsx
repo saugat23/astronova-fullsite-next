9b40108911be0f4b179ec519954c2431b6b28fcf
saugat23 <saugatbhandari8@gmail.com>
2024-04-15 10:39:45 +0545

    refresh

--- src/app/(primary)/layout.jsx
+++ src/app/(primary)/layout.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
-import Navbar from "../../sections/Navbar"
+import Navbar from "../../sections/Navbar";
+import Footer from "../../sections/Footer";
 
 const layout = ({children}) => {
   return (
     <>
         <Navbar />
         {children}
+        <Footer />
     </>
   )
 }
