497a2df06b0a7d0afe34d4fa308ee1411ee81bfa
saugat23 <saugatbhandari8@gmail.com>
2024-05-12 14:53:13 +0545

    refresh

--- src/app/working_area/page.jsx
+++ src/app/working_area/page.jsx
@@ -1,54 +1,57 @@
-"use client"
+"use client";
 
-import React from 'react';
-import { useSearchParams } from 'next/navigation';
-import dynamic from 'next/dynamic'
-import { Suspense } from 'react';
+import React from "react";
+import { useSearchParams } from "next/navigation";
+import dynamic from "next/dynamic";
+import { Suspense } from "react";
 
 const ASW = dynamic(() => import("../../components/UI/WorkingField/ASW"), {
-    loading: ()=> <p> </p>
+  loading: () => <p> </p>,
 });
 
-const Startup = dynamic(() => import("../../components/UI/WorkingField/Startup"), {
-    loading: ()=> <p> </p>
-});
+const Startup = dynamic(
+  () => import("../../components/UI/WorkingField/Startup"),
+  {
+    loading: () => <p> </p>,
+  }
+);
 
 const NESEP = dynamic(() => import("../../components/UI/WorkingField/NESEP"), {
-    loading: ()=> <p> </p>
+  loading: () => <p> </p>,
 });
 
-const Navbar = dynamic(() => import("../../sections/Navbar"),{
-  loading: () => <p> </p> 
+const Navbar = dynamic(() => import("../../sections/Navbar"), {
+  loading: () => <p> </p>,
 });
 
-const Footer = dynamic(() => import("../../sections/Footer"),{
-  loading: () => <p> </p>
+const Footer = dynamic(() => import("../../sections/Footer"), {
+  loading: () => <p> </p>,
 });
 
 const Page = () => {
   const workParams = useSearchParams();
-  const work = workParams.get('useSearchParams hook error');
+  const work = workParams.get("useSearchParams hook error");
 
-  function RenderWorkSection(){
-    if(work == "asw"){
-      return <ASW />
-    }else if(work == "startup"){
-      return <Startup />
-    }else if(work == "nesep"){
-      return <NESEP />
-    }else{
+  function RenderWorkSection() {
+    if (work == "asw") {
+      return <ASW />;
+    } else if (work == "startup") {
+      return <Startup />;
+    } else if (work == "nesep") {
+      return <NESEP />;
+    } else {
       return "some error";
     }
   }
   return (
     <>
-    <Navbar />
-    <Suspense>
-      <RenderWorkSection />
-    </Suspense>
-    <Footer />
+      <Suspense>
+        <Navbar />
+        <RenderWorkSection />
+        <Footer />
+      </Suspense>
     </>
-  )
-}
+  );
+};
 
-export default Page
+export default Page;
--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -1,3 +1,5 @@
+"use client"
+
 import React from "react";
 import Link from "next/link";
 import { FaFacebook, FaInstagram } from "react-icons/fa";
